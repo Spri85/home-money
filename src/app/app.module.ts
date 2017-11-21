88f6c6842c9bdeeba9e25e3a0e1fd3129d73860b
Viktor <viktorpar@gmail.com>
2017-11-21 16:34:38 +0200

    aded logic to login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AuthModule} from './auth/auth.module';
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
+import {UsersService} from './shared/services/users.service';
+import {AuthService} from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AuthModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UsersService} from '../../shared/services/users.service';
+import {User} from '../../shared/models/user.model';
+import {Message} from '../../shared/models/message.model';
+import {AuthService} from '../../shared/services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'spr-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  message: Message;
+
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    this.message = new Message('danger', '');
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+  }
+
+  private showMessage(text: string, type: string = 'danger') {
+  this.message = new Message(type, text);
+  window.setTimeout(() => {
+    this.message.text = '';
+  }, 3000);
+  }
+  onSubmit() {
+  const formData = this.form.value;
+
+  this.userService.getUserByEmail(formData.email)
+    .subscribe((user: User) => {
+    if (user) {
+      if (user.password === formData.password) {
+        // logic
+        this.message.text = '';
+        window.localStorage.setItem('user', JSON.stringify(user));
+        this.authService.login();
+        // this.router.navigate(['']);
+
+      } else {
+        this.showMessage('Passvord is incorrect');
+      }
+    } else {
+      this.showMessage('User no found');
+    }
+    });
+  }
+
+}
