797564db3c19a141cc6e1e5986eca6d08a473c1c
Viktor <viktorpar@gmail.com>
2017-11-22 09:47:24 +0200

    registration

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,7 +4,7 @@ import {UsersService} from '../../shared/services/users.service';
 import {User} from '../../shared/models/user.model';
 import {Message} from '../../shared/models/message.model';
 import {AuthService} from '../../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'spr-login',
@@ -19,19 +19,30 @@ export class LoginComponent implements OnInit {
   constructor(
     private userService: UsersService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
     ) { }
 
   ngOnInit() {
     this.message = new Message('danger', '');
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if ( params['nowCanLogin'] ) {
+        this.showMessage({
+          text: 'Now you can enter to system',
+          type:  'success'
+        });
+      }
+    });
+
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
     });
   }
 
-  private showMessage(text: string, type: string = 'danger') {
-  this.message = new Message(type, text);
+  private showMessage(message: Message) {
+  this.message = message;
   window.setTimeout(() => {
     this.message.text = '';
   }, 3000);
@@ -50,10 +61,16 @@ export class LoginComponent implements OnInit {
         // this.router.navigate(['']);
 
       } else {
-        this.showMessage('Passvord is incorrect');
+        this.showMessage({
+          text: 'Passvord is incorrect',
+          type: 'danger'
+        });
       }
     } else {
-      this.showMessage('User no found');
+      this.showMessage({
+        text: 'User no found',
+        type: 'danger'
+      });
     }
     });
   }
