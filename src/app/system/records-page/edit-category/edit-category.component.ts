c68fea6fc058e7bf6cfbc1d5a049706c6e350329
Viktor <viktorpar@gmail.com>
2017-11-25 11:27:19 +0200

    completed records page

--- src/app/system/records-page/edit-category/edit-category.component.ts
+++ src/app/system/records-page/edit-category/edit-category.component.ts
@@ -1,16 +1,17 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Category} from '../../shared/models/category.model';
-import {Observable} from 'rxjs/Observable';
 import {CategoriesService} from '../../shared/services/categories.service';
 import {Message} from '../../../shared/models/message.model';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'spr-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.scss']
 })
-export class EditCategoryComponent implements OnInit {
+export class EditCategoryComponent implements OnInit, OnDestroy {
+  sub1: Subscription;
 
   @Input() categories: Category[] = [];
   @Output() onCategoryEdit = new EventEmitter<Category>();
@@ -39,13 +40,16 @@ export class EditCategoryComponent implements OnInit {
 
     const category = new Category(name, capacity, +this.currentCategoryId)
 
-    this.categoriesService.updateCategory(category)
+    this.sub1 = this.categoriesService.updateCategory(category)
       .subscribe((category: Category) => {
       this.onCategoryEdit.emit(category);
       this.message.text = 'Категория успешно отредактирована.';
-      console.log(this.message);
       window.setTimeout(() => this.message.text = '', 5000 );
       });
+  }
+
+  ngOnDestroy() {
+    if (this.sub1) this.sub1.unsubscribe();
   }
 
 
---
+++ src/app/system/shared/services/events.service.ts
@@ -0,0 +1,16 @@
+import {BaseApi} from '../../../shared/core/base-api';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {SPREvent} from '../models/event.model';
+
+@Injectable()
+export class EventsService extends BaseApi {
+  constructor(public http: Http) {
+    super(http);
+  }
+
+  addEvent(event: SPREvent): Observable<SPREvent> {
+    return this.post('events', event);
+  }
+}
